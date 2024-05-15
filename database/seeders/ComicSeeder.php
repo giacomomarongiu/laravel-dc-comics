cc55e88666223457b1ec0f7dfbbcba37a8e83eab
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-15 18:58:34 +0200

    [Working] Seeder created  and dumped

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Comic;
 
 class ComicSeeder extends Seeder
 {
@@ -12,6 +12,23 @@ class ComicSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        // Importing my array
+        $comics = config('comics');
+        // Let's see it with php artisan db:seed --class=ComicSeeder
+        //dd($comics);
+
+        foreach ($comics as $comic) {
+            
+            // New istance
+            $newComic = new Comic();
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+            $newComic->price = $comic['price'];
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+            $newComic->save();
+        }
     }
 }
