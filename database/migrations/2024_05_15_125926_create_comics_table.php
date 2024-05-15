c30266a6bc8c96439fe7c38eea392c0fa654b3c3
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-05-15 15:39:11 +0200

    [Working] Edit Readme file + "db" imported + migration compiled

--- database/migrations/2024_05_15_125926_create_comics_table.php
+++ database/migrations/2024_05_15_125926_create_comics_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('thumb', 255);
+            $table->string('price', 10);
+            $table->string('series', 30);
+            $table->date('sale_date');
+            $table->string('type', 30);
             $table->timestamps();
         });
     }
